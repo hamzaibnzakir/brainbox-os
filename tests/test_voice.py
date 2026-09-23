895f70e3920c7028bd61707745ff67caf41ccc2b
root <root@vmi3506076.contaboserver.net>
2026-09-23 14:11:27 +0200

    test: remove deleted Kokoro coverage

--- tests/test_voice.py
+++ tests/test_voice.py
@@ -9,8 +9,3 @@ def test_voice_session_interrupts_speech():
     assert session.speaking is False
     assert session.listening is True
 
-
-def test_kokoro_text_cleanup():
-    from brainbox_os.kokoro_tts import KokoroSynthesizer
-    assert KokoroSynthesizer.clean_text('**Hello**, `boss`!') == 'Hello, boss!'
-    assert KokoroSynthesizer.clean_text('a\n\n b') == 'a b'
