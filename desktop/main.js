e5d7ccb68930484b0bc4e9b194d2c1c40c1d2f34
root <root@vmi3506076.contaboserver.net>
2026-09-24 02:57:39 +0200

    fix: restore real wake session and enable AEC

--- desktop/main.js
+++ desktop/main.js
@@ -55,7 +55,7 @@ function startBrainboxRuntime(){
 
   brainboxProcess = spawn(
     pythonPath(),
-    ['-m','brainbox_os.cli','--dev'],
+    ['-m','brainbox_os.cli','--voice'],
     {
       cwd:path.join(__dirname,'..'),
       windowsHide:true,
